a95d1769bbcf84a1a268fdde6644688434d32b4a
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-21 23:42:48 +0330

    advanced UserBuilder

---
+++ DesignPatterns.Builder/FluentUserBuilder.cs
@@ -0,0 +1,93 @@
+﻿namespace DesignPatterns.Builder;
+
+public class FluentUserBuilder :
+	IPostalCodeSelectionStage,
+	IFirstNameSelectionStage,
+	ILastNameSelectionStage,
+	IBirthDateSelectionStage,
+	ICountrySelectionStage,
+	ICitySelectionStage
+{
+	private string _firstName;
+	private string _lastName;
+	private DateTime _birthDate;
+	private string _country;
+	private string _city;
+	private int _postalCode;
+
+	private FluentUserBuilder()
+	{
+	}
+
+	public static IFirstNameSelectionStage CreateUser()
+	{
+		return new FluentUserBuilder();
+	}
+
+	public ILastNameSelectionStage WithFirstName(string firstName)
+	{
+		_firstName = firstName;
+		return this;
+	}
+
+	public IBirthDateSelectionStage WithLastName(string lastName)
+	{
+		_lastName = lastName;
+		return this;
+	}
+
+	public ICountrySelectionStage WithBirthDate(DateTime birthDate)
+	{
+		_birthDate = birthDate;
+		return this;
+	}
+
+	public ICitySelectionStage WithCountry(string country)
+	{
+		_country = country;
+		return this;
+	}
+
+	public IPostalCodeSelectionStage WithCity(string city)
+	{
+		_city = city;
+		return this;
+	}
+
+	public User WithPostalCode(int postalCode)
+	{
+		_postalCode = postalCode;
+		var address = new Address(_country, _city, _postalCode);
+		return new User(_firstName, _lastName, _birthDate, address);
+	}
+}
+
+public interface IFirstNameSelectionStage
+{
+	public ILastNameSelectionStage WithFirstName(string firstName);
+}
+
+public interface ILastNameSelectionStage
+{
+	public IBirthDateSelectionStage WithLastName(string lastname);
+}
+
+public interface IBirthDateSelectionStage
+{
+	public ICountrySelectionStage WithBirthDate(DateTime birthDate);
+}
+
+public interface ICountrySelectionStage
+{
+	public ICitySelectionStage WithCountry(string country);
+}
+
+public interface ICitySelectionStage
+{
+	public IPostalCodeSelectionStage WithCity(string city);
+}
+
+public interface IPostalCodeSelectionStage
+{
+	public User WithPostalCode(int postalCode);
+}
--- DesignPatterns.Builder/Program.cs
+++ DesignPatterns.Builder/Program.cs
@@ -4,12 +4,21 @@ var user1 = new User("Mohammad", "Hashemi", new DateTime(1997, 11, 21),
 	new Address("Iran", "Ahwaz", 12345));
 Console.WriteLine(user1);
 
-var user2 = new SimpleFluentUserBuilder()
+var userBuilder = new SimpleFluentUserBuilder()
 	.WithFirstName("Reza")
 	.WithLastName("Hashemi")
 	.WithBirthDate(new DateTime(1994, 2, 5))
 	.WithCountry("Iran")
 	.WithCity("Tehran")
-	.WithPostalCode(6789)
-	.Build();
+	.WithPostalCode(6789);
+var user2 = userBuilder.Build();
 Console.WriteLine(user2);
+
+var user3 = FluentUserBuilder.CreateUser()
+	.WithFirstName("Reza")
+	.WithLastName("Hashemi")
+	.WithBirthDate(new DateTime(1994, 2, 5))
+	.WithCountry("Iran")
+	.WithCity("Tehran")
+	.WithPostalCode(6789);
+Console.WriteLine(user3);
