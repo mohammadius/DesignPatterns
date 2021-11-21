19c822fcf047068fb1d38096755d9672ad1394d7
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-21 23:17:30 +0330

    added SimpleFluentUserBuilder

--- DesignPatterns.Builder/Program.cs
+++ DesignPatterns.Builder/Program.cs
@@ -1,4 +1,18 @@
-﻿var user = new User("Mohammad", "Hashemi", new DateTime(1997, 11, 21),
+﻿using DesignPatterns.Builder;
+
+var user = new User("Mohammad", "Hashemi", new DateTime(1997, 11, 21),
 	new Address("Iran", "Ahwaz", 12345));
 
 Console.WriteLine(user);
+
+var userBuilder = new SimpleFluentUserBuilder()
+	.WithFirstName("Reza")
+	.WithLastName("Hashemi")
+	.WithBirthDate(new DateTime(1994, 2, 5))
+	.WithCountry("Iran")
+	.WithCity("Tehran")
+	.WithPostalCode(6789);
+
+var user2 = userBuilder.Build();
+
+Console.WriteLine(user2);
