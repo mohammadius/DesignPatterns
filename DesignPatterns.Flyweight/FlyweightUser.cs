033cacf64648611e5e2c4949153751ea39cb9367
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-22 20:33:27 +0330

    Flyweight User type

---
+++ DesignPatterns.Flyweight/FlyweightUser.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatterns.Flyweight;
+
+public class FlyweightUser
+{
+	private static readonly IList<string> _strings = new List<string>();
+	private readonly IEnumerable<int> _names;
+	public string FullName => string.Join(' ', _names.Select(i => _strings[i]));
+
+	public FlyweightUser(string fullName)
+	{
+		_names = fullName.Split(' ').Select(GetOrAdd);
+	}
+
+	private static int GetOrAdd(string s)
+	{
+		var index = _strings.IndexOf(s);
+		if (index != -1)
+		{
+			return index;
+		}
+
+		_strings.Add(s);
+		return _strings.Count - 1;
+	}
+}
