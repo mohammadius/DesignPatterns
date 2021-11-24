32f62ec88ebee56dc04db6a17362620c62179da4
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-24 18:29:55 +0330

    generic node class

---
+++ DesignPatterns.Iterator/Node.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.Iterator;
+
+public class Node<T>
+{
+	public T Value { get; set; }
+	public Node<T> Left { get; set; }
+	public Node<T> Right { get; set; }
+	public Node<T> Parent { get; set; }
+
+	public Node(T value)
+	{
+		Value = value;
+	}
+	public Node(T value, Node<T> left, Node<T> right)
+	{
+		Value = value;
+		Left = left;
+		Right = right;
+		Left.Parent = Right.Parent = this;
+	}
+}
