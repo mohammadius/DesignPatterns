4964eff3c22ddb7b03e22330536e74fc2055b300
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-24 21:26:30 +0330

    in-order iterator

---
+++ DesignPatterns.Iterator/InOrderIterator.cs
@@ -0,0 +1,59 @@
+﻿namespace DesignPatterns.Iterator;
+
+public class InOrderIterator<T>
+{
+	private readonly Node<T> _root;
+	private bool _yieldedStart;
+	public Node<T> Current { get; set; }
+
+	public InOrderIterator(Node<T> root)
+	{
+		_root = root;
+		Current = root;
+
+		while (Current.Left is not null)
+		{
+			Current = Current.Left;
+		}
+	}
+
+	public bool MoveNext()
+	{
+		if (!_yieldedStart)
+		{
+			_yieldedStart = true;
+			return true;
+		}
+
+		if (Current.Right is not null)
+		{
+			Current = Current.Right;
+			while (Current.Left is not null)
+			{
+				Current = Current.Left;
+			}
+
+			return true;
+		}
+
+		var parent = Current.Parent;
+		while (parent is not null && Current == parent.Right)
+		{
+			Current = parent;
+			parent = parent.Parent;
+		}
+
+		Current = parent;
+		return Current is not null;
+	}
+
+	public void Reset()
+	{
+		Current = _root;
+		while (Current.Left is not null)
+		{
+			Current = Current.Left;
+		}
+		_yieldedStart = false;
+	}
+}
--- DesignPatterns.Iterator/Program.cs
+++ DesignPatterns.Iterator/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// root-> 1
+//     /     \
+//    2       3
+//   / \     / \
+//  4   5   6   7
+
+// in-order: 4 2 5 1 6 3 7
+
+var root = new Node<int>(1,
+	new Node<int>(2,
+		new Node<int>(4), new Node<int>(5)),
+	new Node<int>(3,
+		new Node<int>(6), new Node<int>(7)));
+
+var iterator = new InOrderIterator<int>(root);
+while (iterator.MoveNext())
+{
+	Console.Write(iterator.Current.Value);
+	Console.Write(", ");
+}
