fdcff3db8d44c5503e8d184a6022f5e93c48afec
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-24 21:36:06 +0330

    some documentation

--- DesignPatterns.Iterator/Program.cs
+++ DesignPatterns.Iterator/Program.cs
@@ -17,3 +17,14 @@ foreach (var node in new BinaryTree<int>(root))
 	Console.Write(node.Value);
 	Console.Write(", ");
 }
+
+// foreach -> GetEnumerator() -> {Current, bool MoveNext()}
+
+// https://docs.microsoft.com/en-us/dotnet/csharp/iterators#deeper-dive-into-foreach
+// above code is turned (lowered) into this by compiler:
+// InOrderIterator<int> enumerator = new BinaryTree<int>(root).GetEnumerator();
+// while (enumerator.MoveNext())
+// {
+// 	var node = enumerator.Current;
+// 	Console.WriteLine(node.Value.ToString());
+// }
