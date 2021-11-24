aee36092a5508b5e706ae94770e0520b1f8bab06
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-24 22:29:33 +0330

    flyweight formatted text

---
+++ DesignPatterns.Flyweight/FlyweightFormattedText.cs
@@ -0,0 +1,47 @@
+﻿namespace DesignPatterns.Flyweight;
+
+public class FlyweightFormattedText
+{
+	private readonly string _plainText;
+	private List<TextRange> _formatting = new();
+
+	public FlyweightFormattedText(string plainText)
+	{
+		_plainText = plainText;
+	}
+
+	public TextRange GetRange(int start, int end)
+	{
+		var range = new TextRange { Start = start, End = end };
+		_formatting.Add(range);
+		return range;
+	}
+
+	public override string ToString()
+	{
+		return string.Create(_plainText.Length, _plainText, (span, s) =>
+		{
+			for (var i = 0; i < s.Length; i++)
+			{
+				var c = s[i];
+				foreach (var range in _formatting)
+				{
+					if (range.Covers(i) && range.Capitalize)
+					{
+						c = char.ToUpper(c);
+					}
+				}
+
+				span[i] = c;
+			}
+		});
+	}
+
+	public class TextRange
+	{
+		public int Start { get; set; }
+		public int End { get; set; }
+		public bool Capitalize { get; set; }
+		public bool Covers(int position) => position >= Start && position <= End;
+	}
+}
--- DesignPatterns.Flyweight/Program.cs
+++ DesignPatterns.Flyweight/Program.cs
@@ -1,5 +1,13 @@
-﻿var text = new FormattedText("abcdefghijklmnopqrstuvwxyz");
-text.Capitalize(0, 4);
-text.Capitalize(10, 14);
-text.Capitalize(20, 24);
-Console.WriteLine(text);
+﻿var ft = new FormattedText("abcdefghijklmnopqrstuvwxyz");
+ft.Capitalize(0, 4);
+ft.Capitalize(10, 14);
+ft.Capitalize(20, 24);
+Console.Write("Formatted Text:           ");
+Console.WriteLine(ft);
+
+var fft = new FlyweightFormattedText("abcdefghijklmnopqrstuvwxyz");
+fft.GetRange(0, 4).Capitalize = true;
+fft.GetRange(10, 14).Capitalize = true;
+fft.GetRange(20, 24).Capitalize = true;
+Console.Write("Flyweight Formatted Text: ");
+Console.WriteLine(fft);
