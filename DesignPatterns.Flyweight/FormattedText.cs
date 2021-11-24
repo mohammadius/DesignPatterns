358da70b36d9f371ade10244e3feb44edaf8f850
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-24 22:12:33 +0330

    simple formatted text

---
+++ DesignPatterns.Flyweight/FormattedText.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatterns.Flyweight;
+
+public class FormattedText
+{
+	private readonly string _plainText;
+	private readonly bool[] _capitalize;
+
+	public FormattedText(string plainText)
+	{
+		_plainText = plainText;
+		_capitalize = new bool[plainText.Length];
+	}
+
+	public void Capitalize(int start, int end)
+	{
+		for (int i = start; i <= end; i++)
+		{
+			_capitalize[i] = true;
+		}
+	}
+
+	public override string ToString()
+	{
+		return string.Create(_plainText.Length, _plainText, (span, s) =>
+		{
+			for (var i = 0; i < s.Length; i++)
+			{
+				var c = s[i];
+				span[i] = _capitalize[i] ? char.ToUpper(c) : c;
+			}
+		});
+	}
+}
---
+++ DesignPatterns.Flyweight/Program.cs
@@ -0,0 +1,5 @@
+﻿var text = new FormattedText("abcdefghijklmnopqrstuvwxyz");
+text.Capitalize(0, 4);
+text.Capitalize(10, 14);
+text.Capitalize(20, 24);
+Console.WriteLine(text);
