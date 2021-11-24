7386fe6226c441111b98dc61106d5f60653af6fa
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-24 21:32:13 +0330

    binary tree with in-order GetEnumerator()

--- DesignPatterns.Iterator/BinaryTree.cs
+++ DesignPatterns.Iterator/BinaryTree.cs
@@ -9,35 +9,8 @@ public class BinaryTree<T>
 		_root = root;
 	}
 
-	public IEnumerable<Node<T>> InOrder
+	public InOrderIterator<T> GetEnumerator()
 	{
-		get
-		{
-			foreach (var node in TraverseInOrder(_root))
-			{
-				yield return node;
-			}
-		}
-	}
-
-	private static IEnumerable<Node<T>> TraverseInOrder(Node<T> current)
-	{
-		if (current.Left is not null)
-		{
-			foreach (var left in TraverseInOrder(current.Left))
-			{
-				yield return left;
-			}
-		}
-
-		yield return current;
-
-		if (current.Right is not null)
-		{
-			foreach (var right in TraverseInOrder(current.Right))
-			{
-				yield return right;
-			}
-		}
+		return new InOrderIterator<T>(_root);
 	}
 }
--- DesignPatterns.Iterator/Program.cs
+++ DesignPatterns.Iterator/Program.cs
@@ -12,14 +12,7 @@ var root = new Node<int>(1,
 	new Node<int>(3,
 		new Node<int>(6), new Node<int>(7)));
 
-var iterator = new InOrderIterator<int>(root);
-while (iterator.MoveNext())
-{
-	Console.Write(iterator.Current.Value);
-	Console.Write(", ");
-}
-Console.WriteLine();
-foreach (var node in new BinaryTree<int>(root).InOrder)
+foreach (var node in new BinaryTree<int>(root))
 {
 	Console.Write(node.Value);
 	Console.Write(", ");
